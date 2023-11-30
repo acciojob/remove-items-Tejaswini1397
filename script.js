19724d4b2cf669dce8f96115ccb5047130058f08
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-30 13:27:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-//your JS code here. If required.
-// function removeCol() {
-// 	let x=document.getElementById("colorSelect");
-// 	x.remove(x.selectedIndex);
-// }
-function removeCol(){
-	let x = document.getElementById("colorSelect");
-	x.remove(x.selectedIndex)
+document.addEventListener('DOMContentLoaded', function () {
+  // Attach the removeItem function to the button click event
+  var removeButton = document.querySelector('input[type="button"]');
+  if (removeButton) {
+    removeButton.addEventListener('click', removeItem);
+  }
+});
+
+function removeItem() {
+  // Get the dropdown list by its ID
+  var colorSelect = document.getElementById("colorSelect");
+
+  // Get the selected index
+  var selectedIndex = colorSelect.selectedIndex;
+
+  // Check if an item is selected
+  if (selectedIndex !== -1) {
+    // Remove the selected item
+    colorSelect.remove(selectedIndex);
+  } else {
+    alert("Please select an item before removing.");
+  }
 }
